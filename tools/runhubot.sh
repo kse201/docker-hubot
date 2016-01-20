cf9c0462b7d6b83f58c571f66e717ff497901bbe
kse201 <kse.201@gmail.com>
2016-01-20 11:55:43 +0900

    irc tweak

--- tools/runhubot.sh
+++ tools/runhubot.sh
@@ -7,6 +7,7 @@ export HUBOT_IRC_PORT="6667"
 export HUBOT_IRC_ROOMS="#bot"
 export HUBOT_IRC_USERNAME="hubot"
 export HUBOT_IRC_NICK="hubot"
+export HUBOT_IRC_UNFLOOD=true
 # export HUBOT_IRC_PASSWORD="password"
 
 ./bin/hubot \
